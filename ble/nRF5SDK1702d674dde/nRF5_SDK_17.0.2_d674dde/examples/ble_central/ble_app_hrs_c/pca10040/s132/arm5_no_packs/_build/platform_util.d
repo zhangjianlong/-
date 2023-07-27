192b362aa98c5e4aea23bd8cfea7d883f14b2b86
PROSE\Tony.Zhang <tony.zhang@prosetechnologies.com>
2023-07-27 15:26:39 +0800

    ble

---
+++ ble/nRF5SDK1702d674dde/nRF5_SDK_17.0.2_d674dde/examples/ble_central/ble_app_hrs_c/pca10040/s132/arm5_no_packs/_build/platform_util.d
@@ -0,0 +1,11 @@
+.\_build\platform_util.o: ..\..\..\..\..\..\external\mbedtls\library\platform_util.c
+.\_build\platform_util.o: ..\..\..\..\..\..\external\nrf_tls\mbedtls\nrf_crypto\config\nrf_crypto_mbedtls_config.h
+.\_build\platform_util.o: ..\config\sdk_config.h
+.\_build\platform_util.o: ..\..\..\..\..\..\external\mbedtls\include\mbedtls/check_config.h
+.\_build\platform_util.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\limits.h
+.\_build\platform_util.o: ..\..\..\..\..\..\external\mbedtls\include\mbedtls/platform_util.h
+.\_build\platform_util.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
+.\_build\platform_util.o: ..\..\..\..\..\..\external\mbedtls\include\mbedtls/platform.h
+.\_build\platform_util.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
+.\_build\platform_util.o: ..\..\..\..\..\..\external\mbedtls\include\mbedtls/threading.h
+.\_build\platform_util.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
