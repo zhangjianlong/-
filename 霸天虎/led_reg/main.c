7abe756ecbe2b4c5dfd71f0e906375428bb4b08d
PROSE\Tony.Zhang <tony.zhang@prosetechnologies.com>
2024-02-20 17:26:24 +0800

    led reg

--- 霸天虎/led_reg/main.c
+++ 霸天虎/led_reg/main.c
@@ -1,8 +1,14 @@
 #include "stm32f4xx.h"
 
 int main(){
+	//时钟使能
+	*(unsigned int *)(0x40023800+0x30) |= (1<<5);
+	//GPIOF 配置输出
+	*(unsigned int *)(0x40021400+0x00) &=  ~((0x03)<<(2*6));
+	*(unsigned int *)(0x40021400+0x00) |= (1<<(2*6));
+	//pf6输出高电平
 	*(unsigned int *)(0x40021400+0x14) |= (1<<6);
-	*(unsigned int *)(0x40021400+0x14) |= (1<<6);
+	//pf6输出低电平
 	*(unsigned int *)(0x40021400+0x14) &= ~(1<<6);
 	
 	
